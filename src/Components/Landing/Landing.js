2c0eb74a6a12d07dbd7abcf7e1b9513c7775ffcc
molli1992 <felipe.blaksley@hotmail.com>
2023-02-03 11:03:22 -0300

    Landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,9 +1,38 @@
-function Landing() {
+import { Link } from "react-router-dom";
+import { Button, ButtonGroup } from '@chakra-ui/react';
+
+function LandingPage(props) {
     return (
-        <div>
-            Hola
-        </div>
+
+        <section className="hero">
+
+            <nav>
+
+                <div className="logo"></div>
+
+                <div className="nav-link">
+
+                    <ul>
+                        <Link to={"/home"}>
+                            <ButtonGroup variant='outline' spacing='6'>
+                                <Button color="yellow" _hover={{
+                                    transform: 'scale(1.15)',
+                                }}>Home</Button>
+                            </ButtonGroup>
+                        </Link>
+                    </ul>
+
+                </div>
+
+            </nav>
+
+            <div className="content">
+                <h1>Enjoy your vacation with <br />resort network <span>Ibera</span></h1>
+            </div>
+
+        </section>
+
     )
 };
 
-export default Landing;
+export default LandingPage;

69aa7ee20f447b33266c02ea82c9b8896da665ce
molli1992 <felipe.blaksley@hotmail.com>
2023-02-03 10:59:00 -0300

    landing page

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,9 @@
+function Landing() {
+    return (
+        <div>
+            Hola
+        </div>
+    )
+};
+
+export default Landing;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
-import { store } from "../src/Redux/store/index";
+import store from "./Redux/store/index";
+import { ChakraProvider } from '@chakra-ui/react'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>
+  <ChakraProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function

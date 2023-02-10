7a9dff696a49320e78d4320b8b90f223174ffef1
molli1992 <felipe.blaksley@hotmail.com>
2023-02-10 13:06:24 -0300

    login profile

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -18,6 +18,9 @@ function Login(props) {
         email: "",
         user_password: ""
     });
+    const [dataBaseUsers, SetdataBaseUsers] = useState("");
+    const [error, setError] = useState("");
+    const userArray = [{ name: "tobias", email: "tobias.blaksley@gmail.com", picture: "not found" }];
 
     const handleInputChange = (e) => {
 
@@ -32,13 +35,15 @@ function Login(props) {
 
     const handeleSubmit = (e) => {
 
-        axios.post('/create', input)
-            .then((response) => {
-                console.log(response);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        if (!errorEMsuccessful || !errorPsuccessful) {
+
+            setError("error")
+
+        } else {
+
+            SetdataBaseUsers(userArray)
+
+        }
 
     };
 
@@ -121,7 +126,50 @@ function Login(props) {
 
                 <Button colorScheme='blue'
                     onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-                    Logout
+                    Log out
+                </Button>
+
+            </div>
+
+        )
+
+    } else if (dataBaseUsers.length === 1) {
+
+        return (
+
+            <div className='form'>
+
+                <article key="" className="card">
+
+                    <div className="card-header">
+                        <img src="" alt="picture" />
+                    </div>
+
+                    <div className="card-info">
+                        <ul>
+
+                            <li>
+                                <span>
+                                    Name:
+                                </span>
+                                {dataBaseUsers.name}
+                            </li>
+
+                            <li>
+                                <span>
+                                    Email:
+                                </span>
+                                {dataBaseUsers.email}
+                            </li>
+
+                        </ul>
+                    </div>
+
+                </article>
+
+                <Button colorScheme='blue'
+                    onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                    Log out
                 </Button>
 
             </div>
@@ -209,9 +257,25 @@ function Login(props) {
 
                 <div className="form">
 
+                    <FormControl className="form">
+
+                        {error ? (
+                            <FormHelperText color="green" className="letter">
+                                Error in any of the data provided.
+                            </FormHelperText>
+                        ) : (
+                            <FormErrorMessage></FormErrorMessage>
+                        )}
+
+                    </FormControl>
+
+                </div>
+
+                <div className="form">
+
                     <Stack direction='row' spacing={4} align='center'>
                         <Button colorScheme='teal' variant='solid' onClick={handeleSubmit}>
-                            Submmit
+                            Log in
                         </Button>
                     </Stack>
 

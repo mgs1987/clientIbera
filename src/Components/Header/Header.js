618afd004ac3c7bc6e10be7d3f1a170dc6d978b9
Molli1992 <99288359+Molli1992@users.noreply.github.com>
2023-02-26 14:56:56 -0300

    Update Header.js

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -70,10 +70,6 @@ function Header() {
     var name = user.name;
     var email = user.email;
 
-    console.log("user", user);
-    console.log("name", name);
-    console.log("email", email);
-
     axios
       .get("http://localhost:3010/users")
       .then((res) => {
@@ -81,7 +77,6 @@ function Header() {
         status = res.data.find((u) => {
           return u.email === user.email;
         });
-        console.log("status", status);
 
         if (status.privilige === true) {
           setAdmin("admin");
@@ -94,6 +89,10 @@ function Header() {
       })
       .catch((err) => console.log(err));
   }
+
+  const buttonProfile = (e) => {
+    window.location.href = "http://localhost:3000/profile";
+  };
 
   return (
     <div>
@@ -159,17 +158,33 @@ function Header() {
             )}
 
             {isAuthenticated && admin ? (
-              <Link color="red" fontSize={18} href="/createHotel">
-                Create Hotel{" "}
-              </Link>
-            ) : (
-              <div></div>
-            )}
 
-            {isAuthenticated && admin ? (
-              <Link color="red" fontSize={18} href="/delete">
-                Delete User{" "}
-              </Link>
+              <Popover trigger="hover">
+
+                <PopoverTrigger>
+                  <Link color="red" fontSize={18}>
+                    Admin{" "}
+                  </Link>
+                </PopoverTrigger>
+
+                <PopoverContent>
+
+                  <PopoverHeader>
+                    <Link color="red" fontSize={18} href="/delete">
+                      Administracion{" "}
+                    </Link>
+                  </PopoverHeader>
+
+                  <PopoverHeader>
+                    <Link color="red" fontSize={18} href="/createHotel">
+                      Create{" "}
+                    </Link>
+                  </PopoverHeader>
+
+                </PopoverContent>
+
+              </Popover>
+
             ) : (
               <div></div>
             )}
@@ -195,7 +210,7 @@ function Header() {
             )}
 
             {isAuthenticated ? (
-              <Button colorScheme="teal" variant="outline">
+              <Button colorScheme="teal" variant="outline" onClick={buttonProfile}>
                 {name}
               </Button>
             ) : (
@@ -209,3 +224,4 @@ function Header() {
 }
 
 export default Header;
+

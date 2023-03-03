1748bd054906db99afd67b5f807b641cd60ece94
Mek <99274729+mgs1987@users.noreply.github.com>
2023-03-03 15:39:38 -0300

    (ɔ◔‿◔)ɔ ♥

--- src/Redux/actions/rooms.js
+++ src/Redux/actions/rooms.js
@@ -1,26 +1,29 @@
 import axios from "axios";
 import { GET_ROOMS_CITIES, GET_CLOUDINARY_IMG } from "../actions-types";
-const { REACT_APP_GET_ALL_ROOMS_CITIES, REACT_APP_CLOUDINARY_URL } = process.env;
+const { REACT_APP_GET_ALL_ROOMS_CITIES, REACT_APP_CLOUDINARY_URL } =
+  process.env;
 
 export function getRoomsCities(payload) {
   return async function (dispatch) {
-    console.log('esto es payload: ', payload);
+    console.log("esto es payload: ", payload);
     try {
-      if(!payload){
-        console.log('esto es gallina',);
+      if (!payload) {
+        console.log("esto es gallina");
         let getAllCities = await axios.get(`${REACT_APP_GET_ALL_ROOMS_CITIES}`);
         return dispatch({
           type: GET_ROOMS_CITIES,
           payload: getAllCities.data,
-        })
+        });
       } else {
-        console.log('esto es huevo',);
-        let getAllHotelsCity = await axios.get(`${REACT_APP_GET_ALL_ROOMS_CITIES}?city=${payload}`);
+        console.log("esto es huevo");
+        let getAllHotelsCity = await axios.get(
+          `${REACT_APP_GET_ALL_ROOMS_CITIES}?city=${payload}`
+        );
         return dispatch({
           type: GET_ROOMS_CITIES,
           payload: getAllHotelsCity.data,
-        })
-      } 
+        });
+      }
     } catch (error) {
       console.error(error);
     }

bc5aa057eeed95e40b62545960f3f2f5bbf37f51
Mek <99274729+mgs1987@users.noreply.github.com>
2023-03-03 03:04:59 -0300

    SearchBar fixed ;D
    faaaacil

--- src/Redux/actions-types/index.js
+++ src/Redux/actions-types/index.js
@@ -1,5 +1,4 @@
 export const GET_ALL_HOTELS = "GET_ALL_HOTELS";
-//export const GET_HOTELS="GET_HOTELS";
 export const CITIES = "CITIES";
 export const FILTER_BY_CITY = "FILTER_BY_CITY";
 export const FILTER_BY_STARS = "FILTER_BY_STARS";
@@ -9,7 +8,5 @@ export const GET_ALL_AMENITIES = "GET_ALL_AMENITIES";
 export const CLEAN_FILTER = "CLEAN_FILTER";
 export const CREATE_HOTEL = "CREATE_HOTEL";
 export const GET_NAME_CITIES = "GET_NAME_CITIES";
-export const GET_ROOMS_CITIES = "GET_ROOMS_CITIES";
-export const CREATE_COMMENTS = "CREATE_COMMENTS";
-export const GET_COMMENTS = "GET_COMMENTS";
-// cambios @Facu
+export const REACT_APP_PASS_DATE = "REACT_APP_PASS_DATE";
+export const REACT_APP_TAKE_DATE = "REACT_APP_TAKE_DATE";
